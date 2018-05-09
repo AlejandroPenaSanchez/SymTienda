98eda0705f25570e5c5c2c990fbf2bd7b0be8e6e
Alejandro Pena Sanchez <alejandropenasanchezestiche@gmail.com>
2018-05-09 20:57:24 +0200

    Clase 09-05-2018

--- src/Controller/NoticiasController.php
+++ src/Controller/NoticiasController.php
@@ -6,6 +6,13 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use App\Entity\Noticias;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+
+use Symfony\Component\HttpFoundation\Request;
+
 class NoticiasController extends Controller
 {
     /**
@@ -28,4 +35,49 @@ class NoticiasController extends Controller
 
         ]);
     }
+
+    /**
+     * @Route("/noticias/{id}", name="noticias_detalle")
+     */
+    public function detalle($id)
+    {
+        $repository = $this->getDoctrine()->getRepository(Noticias::class);
+
+        $noticias = $repository->find($id);
+
+        return $this->render('noticias/detalle.html.twig', [
+            'noticia' => $noticias
+        ]);
+    }
+
+    /**
+     * @Route("/noticias_insertar", name="noticias_insertar")
+     */
+    public function insertar(Request $request)
+    {
+        $noticia = new Noticias();
+
+        $form = $this->createFormBuilder($noticia)
+            ->add('titulo', TextType::Class)
+            ->add('descripcion', TextareaType::Class)
+            ->add('autor', TextType::Class)
+            ->add('fecha', DateTimeType::Class)
+            ->add('enviar', SubmitType::Class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $noticia = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($noticia);
+            $entityManager->flush();
+        }
+
+        return $this->render('noticias/insertar.html.twig', [
+            'formulario' => $form->createView()
+        ]);
+    }
+
 }
